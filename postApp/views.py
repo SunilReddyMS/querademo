ae4762ee3befb40699cb8fb52e4827ba274b24eb
sunilkumar <112303785+SunilReddyMS@users.noreply.github.com>
2023-06-13 09:40:54 +0530

    Add files via upload

---
+++ postApp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render,redirect
+from django.views import generic
+
+
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
